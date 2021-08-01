4dd8b6b85dbff73f9648ccb9ce85cc12ad6d9def
Joel Cesar Zamboni <joel.zamboni@gmail.com>
2021-08-01 00:25:47 +0000

    initial commit

--- main.sh
+++ main.sh
@@ -1,5 +1,34 @@
-
 #!/usr/bin/env bash
 
-echo Hello World
-echo $NAME
+# Check if AWS in installed
+hash aws >/dev/null 2>&1 || pip install awscli
+
+# Listing all zones
+ALL_ZONES=$(aws route53 list-hosted-zones-by-name)
+
+
+EXCLUDE_DOMAINS='| select (
+[ -v $EXCLUDE_DOMAINS ] || {
+  for EXCLUDED_DOMAIN in $EXCLUDED_DOMAINS
+  do
+
+  done
+}
+
+# Excluding important zones
+ZONES=$(echo ${ALL_ZONES} | jq -r '.HostedZones[] | 
+select(
+.Name!="" 
+and 
+.Name!="") 
+| .Id')
+
+for ZONE in ${ZONES}
+do
+  ALL_RECORDS=$(aws route53 list-resource-record-sets --hosted-zone-id ${ZONE})
+  # Excluding SOA and NS entries
+  RECORDS=$(echo ${ALL_RECORDS} |  jq -r '.ResourceRecordSets[]| select(.Type!="SOA" and .Type!="NS")')
+  echo $RECORDS | jq '.| legth'
+  read pn
+done
+
