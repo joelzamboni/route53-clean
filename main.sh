de6f1991993396fb25ce774d3ad4ed820f7429c7
Joel Cesar Zamboni <joel.zamboni@gmail.com>
2021-08-03 03:14:20 +0000

    Finished the script

--- main.sh
+++ main.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+set -xe
+
 # Check if AWS in installed
-{ 
-  hash aws || pip install awscli 
-} >/dev/null 2>&1
+hash aws >/dev/null 2>&1 || pip install awscli 
+
 
 # Listing all zones
 ALL_ZONES=$(aws route53 list-hosted-zones-by-name)
@@ -24,30 +25,15 @@ fi
 # Get hosted zones ids
 ZONES=$(echo ${ALL_ZONES} | jq -r ".HostedZones[] | $EXCLUDE_DOMAINS | .Id")
 
-EXCLUDE_TYPES='select (.Type!="SOA" and .Type!="NS")'
-
 for ZONE in ${ZONES}
 do
-  ALL_RECORDS=$(aws route53 list-resource-record-sets --hosted-zone-id ${ZONE})
-  # Excluding SOA and NS entries
-  aws route53 change-resource-record-sets \
-    --hosted-zone-id ${ZONE} \
-    --change-batch file://<(echo ${ALL_RECORDS} | \
-  jq ".ResourceRecordSets[] | 
-  $EXCLUDE_TYPES |
-  {
-    \"Changes\": 
-    [
-      {
-        \"Action\": \"DELETE\",
-        \"ResourceRecordSet\":
-          {
-            Name,
-            Type,
-            TTL,
-            ResourceRecords
-          }
-      }
-    ]
-  } ")
+  ALL_RECORDS=$(aws route53 list-resource-record-sets --hosted-zone-id ${ZONE} | \
+	  jq '[.ResourceRecordSets[] | select (.Type!="SOA" and .Type!="NS") ] | 
+    map ({ "Action": "DELETE", "ResourceRecordSet": . }) | {"Changes": . } ')
+  if [[ "$(echo $ALL_RECORDS | jq length)" -gt "1" ]]
+  then
+    aws route53 change-resource-record-sets --hosted-zone-id ${ZONE} --change-batch file://<(echo ${ALL_RECORDS})
+  else
+    aws route53 delete-hosted-zone --id ${ZONE}
+  fi
 done
